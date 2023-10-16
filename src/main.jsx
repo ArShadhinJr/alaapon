e6a132447839b80e6d55e2e39d7b0d611e57692d
AbdurRahman <arshadhinjr@gmail.com>
2023-10-16 22:26:46 +0600

    add: login and firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
 import App from './App.jsx'
-import './index.css'
-import firebaseConfig from './Auth/firebase'
-import { BrowserRouter, Route,  Routes } from 'react-router-dom'
-import Login from './Components/Login/Login';
-import Registration from './Components/Registration/Registration';
-import 'react-toastify/dist/ReactToastify.css';
 import Home from './Components/Home/Home.jsx'
+import Login from './Components/Login/Login'
+import Registration from './Components/Registration/Registration'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
